41091697c5b3d25667526226d921fbcf20da1f8a
Devanshu Shah <devanshushah294@gmail.com>
2024-08-01 10:13:06 +0530

    update Window functions

--- Advanced SQL/Functions/WindowFunctions.sql
+++ Advanced SQL/Functions/WindowFunctions.sql
@@ -3,7 +3,7 @@ use Learning
 
 Select
 	Emp_ID
-	, Salary
+	,Salary
 	,Dept_id
 	,Sum(Salary) over (Partition by Dept_id order by emp_id) as sum_sal
 	,Avg(Salary) over (Partition by Dept_id order by emp_id) as avg_sal
@@ -15,4 +15,33 @@ Select
 	,Dept_id
 	,Sum(Salary) over (Partition by Dept_id) as sum_sal
 	,Avg(Salary) over (Partition by Dept_id) as avg_sal
+FROM Employee
+
+SELECT * FROM Employee
+Select
+	Emp_ID
+	,Salary
+	,Dept_id
+	,RANK() OVER (PARTITION BY DEPT_ID ORDER BY SALARY) as 'RANK'
+FROM Employee
+
+Select
+	Emp_ID
+	,Salary
+	,Dept_id
+	,ROW_NUMBER() OVER (PARTITION BY DEPT_ID ORDER BY SALARY) as 'ROW_NUMBER'
+FROM Employee
+
+Select
+	Emp_ID
+	,Salary
+	,Dept_id
+	,DENSE_RANK() OVER (PARTITION BY DEPT_ID ORDER BY SALARY) as 'DENSE_RANK'
+FROM Employee
+
+Select
+	Emp_ID
+	,Salary
+	,Dept_id
+	,NTILE(3) OVER (PARTITION BY DEPT_ID ORDER BY SALARY) as 'NTILE'
 FROM Employee
